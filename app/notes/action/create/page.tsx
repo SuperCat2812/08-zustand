de0802a21c9b19de7c1ae65b01418dd7181e06a0
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-06-16 18:40:47 +0200

    feat: add metadata for Create Note page

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./CreateNote.module.css";
+import { Metadata } from "next";
 
+export const metaData: Metadata = {
+  title: "Create Note",
+  description: "Page of create Note to form",
+  openGraph: {
+    title: "Create Note",
+    description: "Page of create Note to form",
+    url: "http://localhost:3000/notes/filter/all",
+    images:
+      "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg?_gl=1*12iazr6*_gcl_au*NTc5NjMyMzM4LjE3Nzg5NDA5OTk.*_ga*MTIyMjk4MTE4NS4xNzc4ODQzNzA0*_ga_PW0T7S5LDQ*czE3ODE2MTEzMjEkbzEzMCRnMSR0MTc4MTYxMjgxMCRqMjkkbDAkaDA.",
+  },
+};
 interface createNoteProps {
   onClose: () => void;
 }
